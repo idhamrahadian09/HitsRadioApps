eae454379060c0a238450165b4ca50bb3dafe863
idhamrahadian09 <idhamrahadian09@gmail.com>
2019-06-19 23:56:05 +0700

    Initial commit

---
+++ app/src/main/java/com/apps/idhamrahadian/hitsradioapps/view/Home.java
@@ -0,0 +1,15 @@
+package com.apps.idhamrahadian.hitsradioapps.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.idhamrahadian.hitsradioapps.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
